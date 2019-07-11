da1d4da618a88c6b3b1247e577b3f2fe7fd4daaf
TevinAchong <tevinachong@hotmail.com>
2019-07-11 17:20:22 -0400

    plot history of cnn

--- bin/cnn.py
+++ bin/cnn.py
@@ -79,3 +79,25 @@ history = model.fit_generator(
 
 # Saving the model
 model.save('../models/cats_and_dogs_small_dropout.h5')
+
+# Displaying curves of loss and accuracy during training
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+
+plt.show()
