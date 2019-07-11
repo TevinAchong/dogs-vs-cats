c76e04bf0840417f3b9d117d86e1f54fafa15cb8
TevinAchong <tevinachong@hotmail.com>
2019-07-11 17:16:15 -0400

    cnn using augmented data

--- bin/cnn.py
+++ bin/cnn.py
@@ -10,7 +10,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 import matplotlib.pyplot as plt
 
-# Instantiating a convnet for classification of dogs vs cats
+# Convnet that includes dropout
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',
                         input_shape=(150, 150, 3)))
@@ -22,6 +22,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -29,15 +30,26 @@ model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', metrics=['accuracy'],
               optimizer=optimizers.RMSprop(lr=1e-4))
 
+# Directories where the training set and validation set are located
+train_dir = '/home/tevinachong/Code/deep_learning_with_python/dogs_vs_cats/minimized_data/train'
+validation_dir = '/home/tevinachong/Code/deep_learning_with_python/dogs_vs_cats/minimized_data/validation'
+
+# Training the convnet using data augmentation generators
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+)
+
 # Data Preprocessing
 # Using keras built in utilities to convert image files
 # into batches of preprocessed tensors
 train_datagen = ImageDataGenerator(rescale=1./255) # Rescales images by 1/255
 test_datagen = ImageDataGenerator(rescale=1./255)
-
-# Directories where the training set and validation set are located
-train_dir = '/home/tevinachong/Code/deep_learning_with_python/dogs_vs_cats/minimized_data/train'
-validation_dir = '/home/tevinachong/Code/deep_learning_with_python/dogs_vs_cats/minimized_data/validation'
 
 '''
 Creates batches of size 20, each containing tensor version of the images in the dataset
@@ -57,48 +69,13 @@ validation_generator = test_datagen.flow_from_directory(
     class_mode='binary'
 )
 
-# Fitting the model using a batch generator
 history = model.fit_generator(
     train_generator,
     steps_per_epoch=100,
-    epochs=30,
+    epochs=100,
     validation_data=validation_generator,
-    validation_steps=50
+    validation_steps=50,
 )
 
 # Saving the model
-model.save('../models/cats_and_dogs_small_1.h5')
-
-# Displaying curves of loss and accuracy during training
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1, len(acc) + 1)
-
-plt.plot(epochs, acc, 'bo', label='Training Accuracy')
-plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.legend()
-
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training Loss')
-plt.plot(epochs, val_loss, 'b', label='Validation Loss')
-plt.title('Training and Validation Loss')
-plt.legend()
-
-plt.show()
-
-# Setting up data augmentation configuration via ImageDataGenerator
-datagen = ImageDataGenerator(
-    rotation_range=40, # range to randomly rotate pictures
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest' # strategy used for filling in newly created pixels
-)
-
+model.save('../models/cats_and_dogs_small_dropout.h5')
